c25a3f25badcbae8243378a575bc0af6ecef49f7
xyz-elyxion <xyz-elyxion@outlook.com>
2026-09-04 22:19:44 +0000

    D

--- bin/elyxion.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-set "SCRIPT_DIR=%~dp0"
-if exist "%SCRIPT_DIR%..\build\Release\elyxion.exe" (
-  "%SCRIPT_DIR%..\build\Release\elyxion.exe" %*
-) else (
-  "%SCRIPT_DIR%elyxion.exe" %*
-)
-exit /b %ERRORLEVEL%
--- examples/hello.js
+++
@@ -1,8 +0,0 @@
-// Hello World Example
-'use strict';
-
-console.log('Hello, World!');
-console.log('Welcome to Elyxion v1.1.0');
-console.log('Platform:', process.platform);
-console.log('Architecture:', process.arch);
-console.log('V8 Version:', process.versions?.v8);
